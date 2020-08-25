a1cb22a0c42c611736bacb947ab08313c9880f68
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-25 08:44:39 -0600

    Add project files.

---
+++ Constants.vb
@@ -0,0 +1,19 @@
+﻿Option Strict On
+Option Explicit On
+
+Module Constants
+
+    Sub Main()
+        Const aNumber As Integer = 5
+
+        'Robotics style convention
+        'All caps case
+        'Include type character
+        Const ANOTHERNUMBER As Integer = 61
+
+        Console.WriteLine(ANOTHERNUMBER)
+        Console.WriteLine(aNumber)
+        Console.WriteLine.Read()
+    End Sub
+
+End Module
